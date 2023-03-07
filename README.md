c6a60ee1f2f4df8bec4526cdb90c2f3bd0d78633
jermb <77866654+jermb@users.noreply.github.com>
2023-03-07 13:13:00 -0600

    Add comments and Update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FizzBuzz
+
+## Branches
+
+    -   `first_pass` is a very basic FizzBuzz program
+    -   `improvements` is a refined version of the program
+    -   `clean_up` adds comments to make code legible
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-for num in range(1, 101):
-    text = ""
+for num in range(1, 101):                   #   Loops through all number from 1 to 100
+    text = ""                               #   Creates an empty string to push text into
 
-    if (num % 3 == 0): text += "Fizz"
-    if (num % 5 == 0): text += "Buzz"
+    if (num % 3 == 0): text += "Fizz"       #   Check if number is divisible by 3: Add Fizz to string
+    if (num % 5 == 0): text += "Buzz"       #   Check if divisible by 5: Add Buzz
+                                            #   If divisible by 3 & 5 string will = "FizzBuzz"
 
-    print(text if text != "" else str(num))
+    print(text if text != "" else str(num)) #   If text is not empty (Fizz and/or Buzz have been added) Prints the text string
+                                            #   Otherwise prints the current number
