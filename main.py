c0061312aeee000f1c225d1b95a8a948a318781e
jermb <77866654+jermb@users.noreply.github.com>
2023-03-07 13:03:43 -0600

    Simplified code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#improvements
 for num in range(1, 101):
-    if (num % 3 == 0 & num % 5 ==0):
-        print("FizzBuzz")
-    elif (num % 3 == 0):
-        print("Fizz")
-    elif (num % 5 == 0):
-        print("Buzz")
+    text = ""
+
+    if (num % 3 == 0): text += "Fizz"
+    if (num % 5 == 0): text += "Buzz"
+
+    print(text if text != "" else str(num))
